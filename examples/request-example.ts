15c711c46e6523b1b4900a388977edefb67b0fbe
Milan Suk <Milansuk@email.cz>
2023-05-07 15:02:24 +0200

    chore: resource example

--- examples/request-example.ts
+++ examples/request-example.ts
@@ -1,10 +1,9 @@
 import * as Http from "effect-http";
-import fs from "fs";
 
 import * as Context from "@effect/data/Context";
-import * as Data from "@effect/data/Data";
 import * as Duration from "@effect/data/Duration";
 import { pipe } from "@effect/data/Function";
+import * as RA from "@effect/data/ReadonlyArray";
 import * as Effect from "@effect/io/Effect";
 import * as Logger from "@effect/io/Logger";
 import * as LogLevel from "@effect/io/Logger/Level";
@@ -12,34 +11,28 @@ import * as Request from "@effect/io/Request";
 import * as RequestResolver from "@effect/io/RequestResolver";
 import * as Schema from "@effect/schema/Schema";
 
-interface NotFoundError extends Data.Case {
-  readonly _tag: "NotFoundError";
-}
-const NotFoundError = Data.tagged<NotFoundError>("NotFoundError");
+import { FileNotFoundError, readFile } from "./_utils";
 
-interface GetValue extends Request.Request<NotFoundError, string> {
+interface GetValue extends Request.Request<FileNotFoundError, string> {
   readonly _tag: "GetValue";
 }
 const GetValue = Request.tagged<GetValue>("GetValue");
 
 const GetValueCache = Context.Tag<Request.Cache>();
 
-const readFileString = (file: string) =>
-  pipe(
-    Effect.async<never, NotFoundError, string>((cb) =>
-      fs.readFile(file, { encoding: "utf8" }, (error, content) => {
-        if (error !== null) {
-          cb(Effect.fail(NotFoundError()));
-        } else {
-          cb(Effect.succeed(content));
-        }
-      }),
-    ),
-    Effect.tap(() => Effect.logDebug(`Content read from ${file}`)),
-  );
-
 const GetValueResolver = RequestResolver.fromFunctionEffect((_: GetValue) =>
-  readFileString("test-file"),
+  pipe(
+    readFile("test-file"),
+    Effect.tap(() => Effect.logDebug("Value read from file")),
+  ),
+);
+
+const requestMyValue = Effect.flatMap(GetValueCache, (getValueCache) =>
+  pipe(
+    Effect.request(GetValue({}), GetValueResolver),
+    Effect.withRequestCache(getValueCache),
+    Effect.withRequestCaching("on"),
+  ),
 );
 
 const api = pipe(
@@ -52,13 +45,7 @@ const server = pipe(
   Http.handle("getValue", () =>
     Effect.flatMap(GetValueCache, (getValueCache) =>
       pipe(
-        Effect.allPar([
-          Effect.request(GetValue({}), GetValueResolver),
-          Effect.request(GetValue({}), GetValueResolver),
-          Effect.request(GetValue({}), GetValueResolver),
-          Effect.request(GetValue({}), GetValueResolver),
-          Effect.request(GetValue({}), GetValueResolver),
-        ]),
+        Effect.allPar(RA.replicate(requestMyValue, 10)),
         Effect.mapError(() => Http.notFoundError("File not found")),
         Effect.withRequestCache(getValueCache),
         Effect.withRequestCaching("on"),
