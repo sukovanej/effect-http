48e369cf7083277d12e0402db7e44f6506b221d9
Milan Suk <Milansuk@email.cz>
2023-05-11 20:40:21 +0200

    chore: example schema with optional field

---
+++ examples/schema-with-optional-field.ts
@@ -0,0 +1,27 @@
+import * as Http from "effect-http";
+
+import { pipe } from "@effect/data/Function";
+import * as Option from "@effect/data/Option";
+import * as Effect from "@effect/io/Effect";
+import * as Schema from "@effect/schema/Schema";
+
+const Response = Schema.struct({
+  foo: Schema.optional(Schema.string).toOption(),
+  bar: Schema.option(Schema.string),
+});
+
+const api = pipe(
+  Http.api(),
+  Http.get("hello", "/hello", {
+    response: Response,
+  }),
+);
+
+const server = pipe(
+  api,
+  Http.server,
+  Http.handle("hello", () => Effect.succeed({ foo: Option.none(), bar: Option.none() })),
+  Http.exhaustive,
+);
+
+pipe(server, Http.listen({ port: 4000 }), Effect.runPromise);
