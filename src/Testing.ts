d704fe081356fa6c9931a15706f4978256047967
Milan Suk <Milansuk@email.cz>
2023-06-17 11:56:10 +0200

    feat: `Testing` module

---
+++ src/Testing.ts
@@ -0,0 +1,112 @@
+/**
+ * Testing if the `Server` implementation.
+ *
+ * @since 1.0.0
+ */
+import { pipe } from "@effect/data/Function";
+import * as Effect from "@effect/io/Effect";
+import type * as Schema from "@effect/schema/Schema";
+
+import type { Api } from "effect-http/Api";
+import type {
+  EndpointSchemasToInput,
+  SelectEndpointById,
+  Server,
+} from "effect-http/Server";
+import { createInputParser } from "effect-http/internal/client";
+
+/** @ignore */
+type MakeHeadersOptionIfAllPartial<I> = I extends { headers: any }
+  ? Schema.Spread<
+      (Record<string, never> extends I["headers"]
+        ? { headers?: I["headers"] }
+        : Pick<I, "headers">) &
+        Omit<I, "headers">
+    >
+  : I;
+
+/**
+ * @category models
+ * @since 1.0.0
+ */
+type TestClientFunction<I> = Record<string, never> extends I
+  ? (input?: I) => Effect.Effect<never, unknown, Response>
+  : (input: I) => Effect.Effect<never, unknown, Response>;
+
+/**
+ * @category models
+ * @since 1.0.0
+ */
+export type TestingClient<A extends Api> = A extends Api<infer Es>
+  ? Schema.Spread<{
+      [Id in Es[number]["id"]]: TestClientFunction<
+        MakeHeadersOptionIfAllPartial<
+          EndpointSchemasToInput<SelectEndpointById<Es, Id>["schemas"]>
+        >
+      >;
+    }>
+  : never;
+
+/**
+ * Create a testing client for the `Server`. It generates a similar interface
+ * as the `Http.client` but instead of remote Http calls it performs direct
+ * handler invocations and returns a `Response` object.
+ *
+ * All the validations and `Request` / `Response` conversions are actually
+ * triggered, the network part is bypassed.
+ *
+ * Server dependencies are propagated to the `Effect` context. Thus, if your
+ * server implementation involves the usage of services, you need to
+ * satisfy the conctract in your tests.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export const testingClient = <R, A extends Api>(
+  server: Server<R, [], A>,
+): Effect.Effect<R, never, TestingClient<A>> =>
+  pipe(
+    Effect.runtime<R>(),
+    Effect.map((runtime) =>
+      server.api.endpoints.reduce((client, { id, method, path, schemas }) => {
+        const parseInputs = createInputParser(schemas);
+
+        const handler = server.handlers.find(
+          ({ endpoint }) => endpoint.id === id,
+        );
+
+        if (handler === undefined) {
+          throw new Error(`Couldn't find server implementation for ${id}`);
+        }
+
+        const fn = (_args: any) => {
+          const args = _args || {};
+
+          return pipe(
+            parseInputs(args),
+            Effect.tap(() => Effect.logTrace(`${method} ${path}`)),
+            Effect.flatMap(({ body, query, params, headers }) => {
+              const pathStr = Object.entries(params ?? {}).reduce(
+                (path, [key, value]) => path.replace(`:${key}`, value as any),
+                path,
+              );
+
+              const url = new URL(`http://localhost${pathStr}`);
+
+              Object.entries(query ?? {}).forEach(([name, value]) =>
+                url.searchParams.set(name, value as any),
+              );
+
+              const init = { body, headers, method };
+              const request = new Request(url, init);
+
+              return handler.fn(request);
+            }),
+            Effect.logAnnotate("clientOperationId", id),
+            Effect.provideContext(runtime.context),
+          );
+        };
+        return { ...client, [id]: fn };
+      }, {} as TestingClient<A>),
+    ),
+  );
---
+++ tests/testing-client.test.ts
@@ -0,0 +1,110 @@
+import * as Context from "@effect/data/Context";
+import { pipe } from "@effect/data/Function";
+import * as Effect from "@effect/io/Effect";
+import * as Schema from "@effect/schema/Schema";
+
+import * as Http from "effect-http";
+
+test("testing query", async () => {
+  const api = pipe(
+    Http.api(),
+    Http.get("hello", "/hello", {
+      response: Schema.string,
+      query: { input: Schema.NumberFromString },
+    }),
+  );
+
+  const server = pipe(
+    api,
+    Http.server,
+    Http.handle("hello", ({ query }) => Effect.succeed(`${query.input + 1}`)),
+  );
+
+  const response = await pipe(
+    Http.testingClient(server),
+    Effect.flatMap((client) => client.hello({ query: { input: 12 } })),
+    Effect.runPromise,
+  );
+
+  expect(await response.json()).toEqual("13");
+});
+
+test("testing failure", async () => {
+  const api = pipe(
+    Http.api(),
+    Http.get("hello", "/hello", {
+      response: Schema.string,
+    }),
+  );
+
+  const server = pipe(
+    api,
+    Http.server,
+    Http.handle("hello", () => Effect.fail(Http.notFoundError("oh oh"))),
+  );
+
+  const response = await pipe(
+    Http.testingClient(server),
+    Effect.flatMap((client) => client.hello({ query: { input: 12 } })),
+    Effect.runPromise,
+  );
+
+  expect(await response.json()).toEqual({
+    error: "NotFoundError",
+    details: "oh oh",
+  });
+  expect(response.status).toEqual(404);
+});
+
+test("testing with dependencies", async () => {
+  const api = pipe(
+    Http.api(),
+    Http.get("hello", "/hello", {
+      response: Schema.string,
+      query: { input: Schema.NumberFromString },
+    }),
+  );
+
+  const MyService = Context.Tag<number>();
+
+  const server = pipe(
+    api,
+    Http.server,
+    Http.handle("hello", ({ query }) =>
+      Effect.map(MyService, (v) => `${query.input + v}`),
+    ),
+  );
+
+  const response = await pipe(
+    Http.testingClient(server),
+    Effect.flatMap((client) => client.hello({ query: { input: 12 } })),
+    Effect.provideService(MyService, 2),
+    Effect.runPromise,
+  );
+
+  expect(await response.json()).toEqual("14");
+});
+
+test("testing params", async () => {
+  const api = pipe(
+    Http.api(),
+    Http.get("hello", "/hello/:input", {
+      response: Schema.string,
+      params: { input: Schema.NumberFromString },
+    }),
+  );
+
+  const server = pipe(
+    api,
+    Http.server,
+    Http.handle("hello", ({ params }) => Effect.succeed(`${params.input + 1}`)),
+  );
+
+  const response = await pipe(
+    Http.testingClient(server),
+    Effect.flatMap((client) => client.hello({ params: { input: 12 } })),
+    Effect.runPromise,
+  );
+
+  expect(await response.json()).toEqual("13");
+});
