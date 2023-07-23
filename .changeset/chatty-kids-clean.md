06b82718dc1b771d9be173eeebb43504536f19b2
Milan Suk <Milansuk@email.cz>
2023-07-23 17:31:21 +0200

    fix: OpenApi required flag

---
+++ .changeset/chatty-kids-clean.md
@@ -0,0 +1,5 @@
+---
+"effect-http": patch
+---
+
+Fix OpenApi required flag.
---
+++ examples/optional-query-parameter.ts
@@ -0,0 +1,44 @@
+import { pipe } from "@effect/data/Function";
+import * as Effect from "@effect/io/Effect";
+import * as Schema from "@effect/schema/Schema";
+
+import * as Http from "effect-http";
+
+const SchemaBooleanFromString = pipe(
+  Schema.literal("true", "false"),
+  Schema.transform(
+    Schema.boolean,
+    (i) => i === "true",
+    (o) => (o ? "true" : "false"),
+  ),
+);
+
+export const api = pipe(
+  Http.api(),
+  Http.get("userById", "/api/users/:userId", {
+    response: Schema.struct({ name: Schema.string }),
+    request: {
+      params: Schema.struct({
+        userId: Schema.string,
+      }),
+      query: Schema.struct({
+        include_deleted: Schema.optional(SchemaBooleanFromString),
+      }),
+      headers: Schema.struct({
+        authorization: Schema.string,
+      }),
+    },
+  }),
+);
+
+const server = pipe(
+  Http.server(api),
+  Http.handle("userById", ({ query: { include_deleted } }) =>
+    Effect.succeed({
+      name: `include_deleted = ${include_deleted ?? "[not set]"}`,
+    }),
+  ),
+  Http.listen({ port: 3000 }),
+);
+
+Effect.runPromise(server);
