4adc5f9b931ff7b6899e80b33063a90d825194ec
Milan Suk <Milansuk@email.cz>
2023-08-08 18:38:46 +0200

    chore: update schema-openapi

--- tests/mock-client.test.ts
+++ tests/mock-client.test.ts
@@ -1,4 +1,5 @@
 import { pipe } from "@effect/data/Function";
+import * as Option from "@effect/data/Option";
 import * as Schema from "@effect/schema/Schema";
 import * as Http from "effect-http";
 
@@ -21,4 +22,21 @@ test("custom response", async () => {
   const response = await runTestEffect(client.getValue({}));
 
   expect(response).toEqual(12);
+});
+
+test("response schema with `optionFromNullable`", async () => {
+  const MySchema = Schema.struct({
+    value: Schema.optionFromNullable(Schema.string),
+  });
+
+  const api = Http.api().pipe(
+    Http.post("test", "/test", {
+      response: MySchema,
+    }),
+  );
+
+  const client = Http.mockClient(api);
+  const response = await runTestEffect(client.test());
+
+  expect(Option.isOption(response.value)).toBe(true);
 });
