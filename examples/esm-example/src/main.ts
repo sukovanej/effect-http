ddba2959c7af85cc93dfc44b7af5228b59b25486
Milan Suk <Milansuk@email.cz>
2023-08-20 15:20:52 +0200

    chore: update esm example

--- examples/esm-example/src/main.ts
+++ examples/esm-example/src/main.ts
@@ -2,13 +2,13 @@ import { Effect } from "effect";
 import * as Http from "effect-http";
 import * as Log from "effect-log";
 
-import { server } from "./server.js";
 import { ItemRepositoryInMemory } from "./repository.js";
+import { server } from "./server.js";
 
 const program = server.pipe(
   Http.listen({ port: 3000 }),
   Effect.provideSomeLayer(Log.setPrettyLogger()),
-  Effect.provideLayer(ItemRepositoryInMemory)
+  Effect.provideLayer(ItemRepositoryInMemory),
 );
 
 Effect.runPromise(program);
