28c2a120b118fdf9c7d29303be9fcbc96aaa21f7
Milan Suk <Milansuk@email.cz>
2023-11-17 14:21:54 +0100

    refactor: middlewares internal

--- src/Middlewares.ts
+++ src/Middlewares.ts
@@ -3,17 +3,10 @@
  *
  * @since 1.0.0
  */
-import * as crypto from "crypto";
-
-import * as Middleware from "@effect/platform/Http/Middleware";
-import * as ServerRequest from "@effect/platform/Http/ServerRequest";
-import * as ServerError from "effect-http/ServerError";
-import * as Effect from "effect/Effect";
-import * as Either from "effect/Either";
-import * as FiberRef from "effect/FiberRef";
-import { pipe } from "effect/Function";
-import * as HashMap from "effect/HashMap";
-import * as Metric from "effect/Metric";
+import type * as App from "@effect/platform/Http/App";
+import type * as ServerError from "effect-http/ServerError";
+import * as internal from "effect-http/internal/middlewares";
+import type * as Effect from "effect/Effect";
 
 /**
  * Add access logs for handled requests. The log runs before each request.
@@ -23,40 +16,24 @@ import * as Metric from "effect/Metric";
  * @category logging
  * @since 1.0.0
  */
-export const accessLog = (level: "Info" | "Warning" | "Debug" = "Info") =>
-  Middleware.make((app) =>
-    pipe(
-      ServerRequest.ServerRequest,
-      Effect.flatMap((request) =>
-        Effect[`log${level}`](`${request.method} ${request.url}`),
-      ),
-      Effect.flatMap(() => app),
-    ),
-  );
+export const accessLog: (
+  level?: "Info" | "Warning" | "Debug",
+) => <R, E>(app: App.Default<R, E>) => App.Default<R, E> = internal.accessLog;
 
 /**
  * Annotate request logs using generated UUID. The default annotation key is `requestId`.
  * The annotation key is configurable using the first argument.
  *
  * Note that in order to apply the annotation also for access logging, you should
- * make sure the `access-log` middleware is plugged after the `uuid-log-annotation`.
+ * make sure the `accessLog` middleware is plugged after the `uuidLogAnnotation`.
  *
  * @category logging
  * @since 1.0.0
  */
-export const uuidLogAnnotation = (logAnnotationKey = "requestId") =>
-  Middleware.make((app) =>
-    pipe(
-      Effect.sync(() => crypto.randomUUID()),
-      Effect.flatMap((uuid) =>
-        FiberRef.update(
-          FiberRef.currentLogAnnotations,
-          HashMap.set<string, unknown>(logAnnotationKey, uuid),
-        ),
-      ),
-      Effect.flatMap(() => app),
-    ),
-  );
+export const uuidLogAnnotation: (
+  logAnnotationKey?: string,
+) => <R, E>(app: App.Default<R, E>) => App.Default<R, E> =
+  internal.uuidLogAnnotation;
 
 /**
  * Measure how many times each endpoint was called in a
@@ -65,22 +42,9 @@ export const uuidLogAnnotation = (logAnnotationKey = "requestId") =>
  * @category metrics
  * @since 1.0.0
  */
-export const endpointCallsMetric = () => {
-  const endpointCalledCounter = Metric.counter("server.endpoint_calls");
-
-  return Middleware.make((app) =>
-    Effect.gen(function* (_) {
-      const request = yield* _(ServerRequest.ServerRequest);
-
-      yield* _(
-        Metric.increment(endpointCalledCounter),
-        Effect.tagMetrics("path", request.url),
-      );
-
-      return yield* _(app);
-    }),
-  );
-};
+export const endpointCallsMetric: () => <R, E>(
+  app: App.Default<R, E>,
+) => App.Default<R, E> = internal.endpointCallsMetric;
 
 /**
  * Logs out a handler failure.
@@ -88,33 +52,8 @@ export const endpointCallsMetric = () => {
  * @category logging
  * @since 1.0.0
  */
-export const errorLog = Middleware.make((app) =>
-  Effect.gen(function* (_) {
-    const request = yield* _(ServerRequest.ServerRequest);
-
-    const response = yield* _(app);
-
-    if (response.status >= 400 && response.status < 500) {
-      yield* _(
-        Effect.logWarning(
-          `${request.method.toUpperCase()} ${request.url} client error ${
-            response.status
-          }`,
-        ),
-      );
-    } else if (response.status >= 500) {
-      yield* _(
-        Effect.logError(
-          `${request.method.toUpperCase()} ${request.url} server error ${
-            response.status
-          }`,
-        ),
-      );
-    }
-
-    return response;
-  }),
-);
+export const errorLog: <R, E>(app: App.Default<R, E>) => App.Default<R, E> =
+  internal.errorLog;
 
 /**
  * @category models
@@ -131,69 +70,13 @@ export interface BasicAuthCredentials {
  * @category authorization
  * @since 1.0.0
  */
-export const basicAuth = <R, _>(
+export const basicAuth: <R2, _>(
   checkCredentials: (
     credentials: BasicAuthCredentials,
-  ) => Effect.Effect<R, ServerError.ServerError, _>,
+  ) => Effect.Effect<R2, ServerError.ServerError, _>,
   options?: Partial<{
     headerName: string;
     skipPaths: readonly string[];
   }>,
-) =>
-  Middleware.make((app) =>
-    Effect.gen(function* (_) {
-      const headerName = options?.headerName ?? "Authorization";
-      const skippedPaths = options?.skipPaths ?? [];
-      const request = yield* _(ServerRequest.ServerRequest);
-
-      if (skippedPaths.includes(request.url)) {
-        return yield* _(app);
-      }
-
-      const authHeader = request.headers[headerName.toLowerCase()];
-
-      if (authHeader === undefined) {
-        return ServerError.unauthorizedError(
-          `Expected header ${headerName}`,
-        ).toServerResponse();
-      }
-
-      const authorizationParts = authHeader.split(" ");
-
-      if (authorizationParts.length !== 2) {
-        return ServerError.unauthorizedError(
-          'Incorrect auhorization scheme. Expected "Basic <credentials>"',
-        ).toServerResponse();
-      }
-
-      if (authorizationParts[0] !== "Basic") {
-        return ServerError.unauthorizedError(
-          `Incorrect auhorization type. Expected "Basic", got "${authorizationParts[0]}"`,
-        ).toServerResponse();
-      }
-
-      const credentialsBuffer = Buffer.from(authorizationParts[1], "base64");
-      const credentialsText = credentialsBuffer.toString("utf-8");
-      const credentialsParts = credentialsText.split(":");
-
-      if (credentialsParts.length !== 2) {
-        return ServerError.unauthorizedError(
-          'Incorrect basic auth credentials format. Expected base64 encoded "<user>:<pass>".',
-        ).toServerResponse();
-      }
-
-      const check = yield* _(
-        checkCredentials({
-          user: credentialsParts[0],
-          password: credentialsParts[1],
-        }),
-        Effect.either,
-      );
-
-      if (Either.isLeft(check)) {
-        return check.left.toServerResponse();
-      }
-
-      return yield* _(app);
-    }),
-  );
+) => <R1, E>(app: App.Default<R1, E>) => App.Default<R1 | R2, E> =
+  internal.basicAuth;
---
+++ src/internal/middlewares.ts
@@ -0,0 +1,154 @@
+/**
+ * Mechanism for extendning behaviour of all handlers on the server.
+ *
+ * @since 1.0.0
+ */
+import * as crypto from "crypto";
+
+import * as Middleware from "@effect/platform/Http/Middleware";
+import * as ServerRequest from "@effect/platform/Http/ServerRequest";
+import type * as Middlewares from "effect-http/Middlewares";
+import * as ServerError from "effect-http/ServerError";
+import * as Effect from "effect/Effect";
+import * as Either from "effect/Either";
+import * as FiberRef from "effect/FiberRef";
+import { pipe } from "effect/Function";
+import * as HashMap from "effect/HashMap";
+import * as Metric from "effect/Metric";
+
+export const accessLog = (level: "Info" | "Warning" | "Debug" = "Info") =>
+  Middleware.make((app) =>
+    pipe(
+      ServerRequest.ServerRequest,
+      Effect.flatMap((request) =>
+        Effect[`log${level}`](`${request.method} ${request.url}`),
+      ),
+      Effect.flatMap(() => app),
+    ),
+  );
+
+export const uuidLogAnnotation = (logAnnotationKey = "requestId") =>
+  Middleware.make((app) =>
+    pipe(
+      Effect.sync(() => crypto.randomUUID()),
+      Effect.flatMap((uuid) =>
+        FiberRef.update(
+          FiberRef.currentLogAnnotations,
+          HashMap.set<string, unknown>(logAnnotationKey, uuid),
+        ),
+      ),
+      Effect.flatMap(() => app),
+    ),
+  );
+
+export const endpointCallsMetric = () => {
+  const endpointCalledCounter = Metric.counter("server.endpoint_calls");
+
+  return Middleware.make((app) =>
+    Effect.gen(function* (_) {
+      const request = yield* _(ServerRequest.ServerRequest);
+
+      yield* _(
+        Metric.increment(endpointCalledCounter),
+        Effect.tagMetrics("path", request.url),
+      );
+
+      return yield* _(app);
+    }),
+  );
+};
+
+export const errorLog = Middleware.make((app) =>
+  Effect.gen(function* (_) {
+    const request = yield* _(ServerRequest.ServerRequest);
+
+    const response = yield* _(app);
+
+    if (response.status >= 400 && response.status < 500) {
+      yield* _(
+        Effect.logWarning(
+          `${request.method.toUpperCase()} ${request.url} client error ${
+            response.status
+          }`,
+        ),
+      );
+    } else if (response.status >= 500) {
+      yield* _(
+        Effect.logError(
+          `${request.method.toUpperCase()} ${request.url} server error ${
+            response.status
+          }`,
+        ),
+      );
+    }
+
+    return response;
+  }),
+);
+
+export const basicAuth = <R, _>(
+  checkCredentials: (
+    credentials: Middlewares.BasicAuthCredentials,
+  ) => Effect.Effect<R, ServerError.ServerError, _>,
+  options?: Partial<{
+    headerName: string;
+    skipPaths: readonly string[];
+  }>,
+) =>
+  Middleware.make((app) =>
+    Effect.gen(function* (_) {
+      const headerName = options?.headerName ?? "Authorization";
+      const skippedPaths = options?.skipPaths ?? [];
+      const request = yield* _(ServerRequest.ServerRequest);
+
+      if (skippedPaths.includes(request.url)) {
+        return yield* _(app);
+      }
+
+      const authHeader = request.headers[headerName.toLowerCase()];
+
+      if (authHeader === undefined) {
+        return ServerError.unauthorizedError(
+          `Expected header ${headerName}`,
+        ).toServerResponse();
+      }
+
+      const authorizationParts = authHeader.split(" ");
+
+      if (authorizationParts.length !== 2) {
+        return ServerError.unauthorizedError(
+          'Incorrect auhorization scheme. Expected "Basic <credentials>"',
+        ).toServerResponse();
+      }
+
+      if (authorizationParts[0] !== "Basic") {
+        return ServerError.unauthorizedError(
+          `Incorrect auhorization type. Expected "Basic", got "${authorizationParts[0]}"`,
+        ).toServerResponse();
+      }
+
+      const credentialsBuffer = Buffer.from(authorizationParts[1], "base64");
+      const credentialsText = credentialsBuffer.toString("utf-8");
+      const credentialsParts = credentialsText.split(":");
+
+      if (credentialsParts.length !== 2) {
+        return ServerError.unauthorizedError(
+          'Incorrect basic auth credentials format. Expected base64 encoded "<user>:<pass>".',
+        ).toServerResponse();
+      }
+
+      const check = yield* _(
+        checkCredentials({
+          user: credentialsParts[0],
+          password: credentialsParts[1],
+        }),
+        Effect.either,
+      );
+
+      if (Either.isLeft(check)) {
+        return check.left.toServerResponse();
+      }
+
+      return yield* _(app);
+    }),
+  );
