4458e5fca6f041974f91f10bdad70b09284316c5
Milan Suk <Milansuk@email.cz>
2023-12-16 12:26:03 +0100

    fix: use `ClientRequestEncoder` for `MockClient`

--- src/internal/mock-client.ts
+++ src/internal/mock-client.ts
@@ -3,6 +3,7 @@ import * as OpenApi from "schema-openapi";
 import type * as Api from "effect-http/Api";
 import type * as Client from "effect-http/Client";
 import type * as MockClient from "effect-http/MockClient";
+import * as ClientRequestEncoder from "effect-http/internal/clientRequestEncoder";
 import * as utils from "effect-http/internal/utils";
 import * as Effect from "effect/Effect";
 import { pipe } from "effect/Function";
@@ -11,17 +12,19 @@ export const make = <Endpoints extends Api.Endpoint>(
   api: Api.Api<Endpoints>,
   option?: Partial<MockClient.Options<Endpoints>>,
 ): Client.Client<Endpoints> =>
-  api.endpoints.reduce((client, { id, schemas }) => {
-    const parseInputs = utils.createRequestEncoder(schemas.request);
-    const responseSchema = utils.createResponseSchema(schemas.response);
+  api.endpoints.reduce((client, endpoint) => {
+    const requestEncoder = ClientRequestEncoder.create(endpoint);
+    const responseSchema = utils.createResponseSchema(
+      endpoint.schemas.response,
+    );
 
     const customResponses = option?.responses;
     const customResponse =
-      customResponses && customResponses[id as Endpoints["id"]];
+      customResponses && customResponses[endpoint.id as Endpoints["id"]];
 
-    const fn = (args: any) => {
+    const fn = (args: unknown) => {
       return pipe(
-        parseInputs(args),
+        requestEncoder.encodeRequest(args),
         Effect.flatMap(() =>
           customResponse !== undefined
             ? Effect.succeed(customResponse)
@@ -30,5 +33,5 @@ export const make = <Endpoints extends Api.Endpoint>(
       );
     };
 
-    return { ...client, [id]: fn };
+    return { ...client, [endpoint.id]: fn };
   }, {} as Client.Client<Endpoints>);
