88f736bf6accac0567d6f16b9a54662d2f3da910
Milan Suk <Milansuk@email.cz>
2024-06-14 10:52:24 +0200

    refactor: move isIgnored impl to internal

--- packages/effect-http/src/internal/api-schema.ts
+++ packages/effect-http/src/internal/api-schema.ts
@@ -1,4 +1,5 @@
 import * as Schema from "@effect/schema/Schema"
+import * as Predicate from "effect/Predicate"
 import * as OpenApiCompiler from "schema-openapi/OpenApiCompiler"
 import type * as ApiSchema from "../ApiSchema.js"
 
@@ -8,6 +9,11 @@ export const IgnoredId: ApiSchema.IgnoredId = Symbol.for(
 
 export const Ignored: ApiSchema.Ignored = { [IgnoredId]: IgnoredId }
 
+/** @internal */
 export const formDataSchema = Schema.instanceOf(FormData, {
   description: "Multipart form data"
 }).pipe(OpenApiCompiler.annotate({}))
+
+/** @internal */
+export const isIgnored = (u: unknown): u is ApiSchema.Ignored =>
+  Predicate.hasProperty(u, IgnoredId) && u[IgnoredId] === IgnoredId
