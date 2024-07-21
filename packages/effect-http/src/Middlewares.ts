621954d011359e10a3ffd5cfbab8a722d222db54
Milan Suk <Milansuk@email.cz>
2024-07-21 22:08:37 +0200

    remove unnecessary middlewares (#631)
    * remove basic auth middleware
    
    * remove endpointCallsMetric middleware
    
    * remove cors middleware

--- packages/effect-http/src/Middlewares.ts
+++ packages/effect-http/src/Middlewares.ts
@@ -4,10 +4,8 @@
  * @since 1.0.0
  */
 import type * as HttpApp from "@effect/platform/HttpApp"
-import type * as Effect from "effect/Effect"
 import type * as LogLevel from "effect/LogLevel"
 
-import type * as HttpError from "./HttpError.js"
 import * as internal from "./internal/middlewares.js"
 
 /**
@@ -37,69 +35,9 @@ export const uuidLogAnnotation: (
 ) => <R, E>(app: HttpApp.Default<E, R>) => HttpApp.Default<E, R> = internal.uuidLogAnnotation
 
 /**
- * Measure how many times each endpoint was called in a
- * `server.endpoint_calls` counter metrics.
- *
- * @category metrics
- * @since 1.0.0
- */
-export const endpointCallsMetric: () => <R, E>(
-  app: HttpApp.Default<E, R>
-) => HttpApp.Default<E, R> = internal.endpointCallsMetric
-
-/**
  * Logs out a handler failure.
  *
  * @category logging
  * @since 1.0.0
  */
 export const errorLog: <R, E>(app: HttpApp.Default<E, R>) => HttpApp.Default<E, R> = internal.errorLog
-
-/**
- * @category models
- * @since 1.0.0
- */
-export interface BasicAuthCredentials {
-  user: string
-  password: string
-}
-
-/**
- * Basic auth middleware.
- *
- * @category authorization
- * @deprecated Use the `Security` module instead
- * @since 1.0.0
- */
-export const basicAuth: <R2, _>(
-  checkCredentials: (
-    credentials: BasicAuthCredentials
-  ) => Effect.Effect<_, HttpError.HttpError, R2>,
-  options?: Partial<{
-    headerName: string
-    skipPaths: ReadonlyArray<string>
-  }>
-) => <R1, E>(app: HttpApp.Default<E, R1>) => HttpApp.Default<E, R1 | R2> = internal.basicAuth
-
-/**
- * @category models
- * @since 1.0.0
- */
-export interface CorsOptions {
-  allowedOrigins: ReadonlyArray<string>
-  allowedMethods: ReadonlyArray<string>
-  allowedHeaders: ReadonlyArray<string>
-  exposedHeaders: ReadonlyArray<string>
-  maxAge: number
-  credentials: boolean
-}
-
-/**
- * Basic auth middleware.
- *
- * @category authorization
- * @since 1.0.0
- */
-export const cors: (
-  options?: Partial<CorsOptions>
-) => <R, E>(app: HttpApp.Default<E, R>) => HttpApp.Default<E, R> = internal.cors
--- packages/effect-http/src/internal/middlewares.ts
+++ packages/effect-http/src/internal/middlewares.ts
@@ -4,21 +4,13 @@
  * @since 1.0.0
  */
 
-import * as Headers from "@effect/platform/Headers"
 import * as HttpMiddleware from "@effect/platform/HttpMiddleware"
 import * as HttpServerRequest from "@effect/platform/HttpServerRequest"
-import * as HttpServerResponse from "@effect/platform/HttpServerResponse"
 import * as Effect from "effect/Effect"
-import * as Either from "effect/Either"
-import * as Encoding from "effect/Encoding"
 import * as FiberRef from "effect/FiberRef"
 import { pipe } from "effect/Function"
 import * as HashMap from "effect/HashMap"
 import * as LogLevel from "effect/LogLevel"
-import * as Metric from "effect/Metric"
-
-import * as HttpError from "../HttpError.js"
-import type * as Middlewares from "../Middlewares.js"
 
 /** @internal */
 export const accessLog = (level: LogLevel.LogLevel = LogLevel.Info) =>
@@ -46,24 +38,6 @@ export const uuidLogAnnotation = (logAnnotationKey = "requestId") =>
   )
 
 /** @internal */
-export const endpointCallsMetric = () => {
-  const endpointCalledCounter = Metric.counter("server.endpoint_calls")
-
-  return HttpMiddleware.make((app) =>
-    Effect.gen(function*(_) {
-      const request = yield* _(HttpServerRequest.HttpServerRequest)
-
-      yield* _(
-        Metric.increment(endpointCalledCounter),
-        Effect.tagMetrics("path", request.url)
-      )
-
-      return yield* _(app)
-    })
-  )
-}
-
-/** @internal */
 export const errorLog = HttpMiddleware.make((app) =>
   Effect.gen(function*(_) {
     const request = yield* _(HttpServerRequest.HttpServerRequest)
@@ -87,201 +61,3 @@ export const errorLog = HttpMiddleware.make((app) =>
     return response
   })
 )
-
-/** @internal */
-export const basicAuth = <R, _>(
-  checkCredentials: (
-    credentials: Middlewares.BasicAuthCredentials
-  ) => Effect.Effect<_, HttpError.HttpError, R>,
-  options?: Partial<{
-    headerName: string
-    skipPaths: ReadonlyArray<string>
-  }>
-) =>
-  HttpMiddleware.make((app) =>
-    Effect.gen(function*(_) {
-      const headerName = options?.headerName ?? "Authorization"
-      const skippedPaths = options?.skipPaths ?? []
-      const request = yield* _(HttpServerRequest.HttpServerRequest)
-
-      if (skippedPaths.includes(request.url)) {
-        return yield* _(app)
-      }
-
-      const authHeader = request.headers[headerName.toLowerCase()]
-
-      if (authHeader === undefined) {
-        return HttpError.unauthorized(
-          `Expected header ${headerName}`
-        ).pipe(HttpError.toResponse)
-      }
-
-      const authorizationParts = authHeader.split(" ")
-
-      if (authorizationParts.length !== 2) {
-        return HttpError.unauthorized(
-          "Incorrect auhorization scheme. Expected \"Basic <credentials>\""
-        ).pipe(HttpError.toResponse)
-      }
-
-      if (authorizationParts[0] !== "Basic") {
-        return HttpError.unauthorized(
-          `Incorrect auhorization type. Expected "Basic", got "${authorizationParts[0]}"`
-        ).pipe(HttpError.toResponse)
-      }
-
-      const credentialsDecoded = Encoding.decodeBase64String(authorizationParts[1])
-
-      if (Either.isLeft(credentialsDecoded)) {
-        return HttpError.unauthorized("Invalid base64 encoding").pipe(HttpError.toResponse)
-      }
-
-      const credentialsText = credentialsDecoded.right
-      const credentialsParts = credentialsText.split(":")
-
-      if (credentialsParts.length !== 2) {
-        return HttpError.unauthorized(
-          "Incorrect basic auth credentials format. Expected base64 encoded \"<user>:<pass>\"."
-        ).pipe(HttpError.toResponse)
-      }
-
-      const check = yield* _(
-        checkCredentials({
-          user: credentialsParts[0],
-          password: credentialsParts[1]
-        }),
-        Effect.either
-      )
-
-      if (Either.isLeft(check)) {
-        return HttpError.toResponse(check.left)
-      }
-
-      return yield* _(app)
-    })
-  )
-
-/** @internal */
-export const cors = (_options?: Partial<Middlewares.CorsOptions>) => {
-  const DEFAULTS = {
-    allowedOrigins: ["*"],
-    allowedMethods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    allowedHeaders: [],
-    exposedHeaders: [],
-    credentials: false
-  } as const
-
-  const options = { ...DEFAULTS, ..._options }
-
-  const isAllowedOrigin = (origin: string) => {
-    return options.allowedOrigins.includes(origin)
-  }
-
-  const allowOrigin = (originHeader: string) => {
-    if (options.allowedOrigins.length === 0) {
-      return { "Access-Control-Allow-Origin": "*" }
-    }
-
-    if (options.allowedOrigins.length === 1) {
-      return {
-        "Access-Control-Allow-Origin": options.allowedOrigins[0],
-        Vary: "Origin"
-      }
-    }
-
-    if (isAllowedOrigin(originHeader)) {
-      return {
-        "Access-Control-Allow-Origin": originHeader,
-        Vary: "Origin"
-      }
-    }
-
-    return undefined
-  }
-
-  const allowMethods = (() => {
-    if (options.allowedMethods.length > 0) {
-      return {
-        "Access-Control-Allow-Methods": options.allowedMethods.join(", ")
-      }
-    }
-
-    return undefined
-  })()
-
-  const allowCredentials = (() => {
-    if (options.credentials) {
-      return { "Access-Control-Allow-Credentials": "true" }
-    }
-
-    return undefined
-  })()
-
-  const allowHeaders = (accessControlRequestHeaders: string | undefined) => {
-    if (options.allowedHeaders.length === 0 && accessControlRequestHeaders) {
-      return {
-        Vary: "Access-Control-Request-Headers",
-        "Access-Control-Allow-Headers": accessControlRequestHeaders
-      }
-    }
-
-    if (options.allowedHeaders) {
-      return {
-        "Access-Control-Allow-Headers": options.allowedHeaders.join(",")
-      }
-    }
-
-    return undefined
-  }
-
-  const exposeHeaders = (() => {
-    if (options.exposedHeaders.length > 0) {
-      return {
-        "Access-Control-Expose-Headers": options.exposedHeaders.join(",")
-      }
-    }
-
-    return undefined
-  })()
-
-  const maxAge = (() => {
-    if (options.maxAge) {
-      return { "Access-Control-Max-Age": options.maxAge.toString() }
-    }
-
-    return undefined
-  })()
-
-  return HttpMiddleware.make((app) =>
-    Effect.gen(function*(_) {
-      const request = yield* _(HttpServerRequest.HttpServerRequest)
-
-      const origin = request.headers["origin"]
-      const accessControlRequestHeaders = request.headers["access-control-request-headers"]
-
-      let corsHeaders = {
-        ...allowOrigin(origin),
-        ...allowCredentials,
-        ...exposeHeaders
-      }
-
-      if (request.method === "OPTIONS") {
-        corsHeaders = {
-          ...corsHeaders,
-          ...allowMethods,
-          ...allowHeaders(accessControlRequestHeaders),
-          ...maxAge
-        }
-
-        return HttpServerResponse.empty({
-          status: 204,
-          headers: Headers.fromInput(corsHeaders)
-        })
-      }
-
-      const response = yield* _(app)
-
-      return response.pipe(HttpServerResponse.setHeaders(corsHeaders))
-    })
-  )
-}
